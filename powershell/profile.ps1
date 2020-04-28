b834b8048c63226efe1538f441d56dbab8550883
chrispalmo <34981948+chrispalmo@users.noreply.github.com>
2020-04-28 22:43:48 +1000

    update profile.ps1

--- powershell/profile.ps1
+++ powershell/profile.ps1
@@ -5,17 +5,35 @@ Function prompt {
 }
 
 # Import custom powershell scripts
-$custom_script_dir = "PATH_TO_YOUR_CUSTOM_PS1_SCRIPT_DIRECTORY"
+$custom_script_dir = "~\dev\code-snippets\powershell\profile-functions\" 
 Write-Output `
 	("*** Importing all .ps1 modules from " `
 	+ $custom_script_dir + `
 	" ***")
+Write-Output ""
 Get-ChildItem `
 	$custom_script_dir `
 	-Filter *.ps1 | `
 	Foreach-Object `
 	{ Import-Module ($custom_script_dir + $_) }
 
-# Navigate to the default startup folder
-cd "C:\PATH\TO\YOUR\PREFERRED\STARTUP\FOLDER\"
+# Quick actions
+function sps {start powershell}
+function spse {start powershell; exit}
 
+# Web Apps
+function mail {start chrome.exe "https://mail.google.com/"}
+function calendar {start chrome.exe "https://calendar.google.com/calendar/"}
+function sheets {start chrome.exe "https://docs.google.com/spreadsheets/"}
+function slides {start chrome.exe "https://docs.google.com/presentation/"}
+function docs {start chrome.exe "https://docs.google.com/document"}
+
+# Folders
+function dev {cd "~\dev\"}
+function dropbox {cd "~\dropbox"}
+function profile {cd "~\Documents\WindowsPowerShell\"}
+function desk {cd "~\Desktop\"}
+function drawer {cd "~\Dropbox\desk-drawer"}
+
+# Navigate to the default startup folder
+dev
