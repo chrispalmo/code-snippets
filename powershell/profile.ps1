293805fa9e7dbf442a07f3dbf76aca65f5446d09
chrispalmo <34981948+chrispalmo@users.noreply.github.com>
2020-04-29 11:31:34 +1000

    update profile.ps1

--- powershell/profile.ps1
+++ powershell/profile.ps1
@@ -20,15 +20,33 @@ Get-ChildItem `
 # Quick actions
 function sps {start powershell}
 function spse {start powershell; exit}
+function server {py -m simpleHTTPServer}
 
 # Web Apps
+
+## Utilities
 function mail {start chrome.exe "https://mail.google.com/"}
 function calendar {start chrome.exe "https://calendar.google.com/calendar/"}
 function sheets {start chrome.exe "https://docs.google.com/spreadsheets/"}
 function slides {start chrome.exe "https://docs.google.com/presentation/"}
 function docs {start chrome.exe "https://docs.google.com/document"}
 
-# Folders
+## Dev
+function github {start chrome.exe "http://github.com/chrispalmo/"}
+function stackoverflow {start chrome.exe "https://stackoverflow.com/"}
+
+## Comms
+function messenger {start chrome.exe "https://www.messenger.com/"}
+function twitter {start chrome.exe "https://twitter.com/"}
+function hootsuite {start chrome.exe "https://hootsuite.com/dashboard#/streams"}
+
+## News
+function ii {start chrome.exe "https://www.intelligentinvestor.com.au/identity/logon?returnUrl=%2F&prefix=2"}
+function reddit {start chrome.exe "https://www.reddit.com/"}
+function ychn {start chrome.exe "https://news.ycombinator.com/"}
+function youtube {start chrome.exe "https://www.youtube.com/"}
+
+# General Folders
 function dev {cd "~\dev\"}
 function dropbox {cd "~\dropbox"}
 function profile {cd "~\Documents\WindowsPowerShell\"}
