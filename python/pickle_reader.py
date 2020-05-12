7d048ee7db79f43e5c966c70234159bc82175acc
chrispalmo <34981948+chrispalmo@users.noreply.github.com>
2020-05-12 11:51:43 +1000

    Update pickle_reader.py

--- python/pickle_reader.py
+++ python/pickle_reader.py
@@ -9,27 +9,34 @@ import argparse
 import pickle
 import numpy
 
-parser = argparse.ArgumentParser(description='Open a Python "Pickle" file, print variable TYPE and CONTENTS')
-parser.add_argument('filename', type=str, nargs='+',
-                   help='path to pickle file')
+def pickle_reader(filename, printout=False):
 
-args = parser.parse_args()
-filename = args.filename[0]
+	with open(filename, 'rb') as pickle_file:
+		pickled_variable = pickle.load(pickle_file)
 
-with open(filename, 'rb') as pickle_file:
-	pickled_variable = pickle.load(pickle_file)
+	if printout:
+		print(f'\nLoaded {filename}.')
 
-print(f'\nLoaded {filename}.')
+		try:
+			print(f'\nType: {type(pickled_variable)}')
+			print(f'\nDimensions {pickled_variable.shape}')
+		except:
+			pass
+		try:
+			print(f'\nContents: ')
+			for row in pickled_variable: 
+				print("\n",row)
+				print("Type: ",type(row))
+				print("Contains: ",type(row[0]),"(based on 0th element)")
+		except:
+			print(pickled_variable)
 
-try:
-	print(f'\nType: ',type(pickled_variable))
-except:
-	pass
-try:
-	print(f'\nContents: ')
-	for row in pickled_variable: 
-		print("\n",row)
-		print("Type: ",type(row))
-		print("Contains: ",type(row[0]),"(based on 0th element)")
-except:
-	print(pickled_variable)
+	return(pickled_variable)
+
+if __name__ == "__main__":
+	parser = argparse.ArgumentParser(description='Open a Python "Pickle" file, print variable TYPE and CONTENTS')
+	parser.add_argument('filename', type=str, nargs='+',
+	                   help='path to pickle file')
+	args = parser.parse_args()
+	filename = args.filename[0]
+	pickle_reader(filename, printout=True)
