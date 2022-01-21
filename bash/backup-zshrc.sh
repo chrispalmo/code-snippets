2ea935889f11296f9a088b136aa8decfbf2f7b0e
chrispalmo <34981948+chrispalmo@users.noreply.github.com>
2022-01-21 16:06:56 +1000

    latest tweaks

---
+++ bash/backup-zshrc.sh
@@ -0,0 +1,4 @@
+  BACKUP_PATH="$HOME"/dev/code-snippets/bash/.zshrc
+  rm $BACKUP_PATH
+  cp "$HOME"/.zshrc $BACKUP_PATH
+
---
+++ bash/restore-zshrc.sh
@@ -0,0 +1,3 @@
+  BACKUP_PATH="$HOME"/dev/code-snippets/bash/.zshrc
+  cp $BACKUP_PATH "$HOME"/.zshrc
+
