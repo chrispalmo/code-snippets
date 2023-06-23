099b5269c44fd8bcffcebb7d68ade941a8b6afd5
chrispalmo <34981948+chrispalmo@users.noreply.github.com>
2023-06-23 23:59:32 +0700

    tweak combine-files.py (less whitespace in output)

--- python/combine-files.py
+++ python/combine-files.py
@@ -18,24 +18,24 @@ def combine_files(target_dir, output_file):
     Example usage:
     python3 combine-files.py target_directory output_file
     """
-
-    types = ('**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx')
+    types = ('**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx') 
     files_grabbed = []
     for files in types:
         files_grabbed.extend(glob.glob(target_dir + files, recursive=True))
 
     with open(output_file, 'w') as outfile:
         for fname in files_grabbed:
-            outfile.write(f'{os.path.basename(fname)}\n')
-            outfile.write('```tsx\n')
+            outfile.write(f'{os.path.basename(fname)}\n```tsx\n')
             with open(fname) as infile:
-                for line in infile:
+                lines = infile.readlines()
+                # Strip leading and trailing newlines
+                lines = [line for line in lines if line.strip()]
+                for line in lines:
                     if not line.strip().startswith('import'):
                         outfile.write(line)
-            outfile.write('```\n\n')
+            outfile.write('```\n')
 
 if __name__ == "__main__":
     target_dir = sys.argv[1]
     output_file = sys.argv[2] if len(sys.argv) > 2 else os.getcwd() + '/combined.md'
     combine_files(target_dir, output_file)
-
